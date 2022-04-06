1afbc2592f145576047d6da4fa550a7ca8711366
sanyas-dada <sanyasdada09@gmail.com>
2022-04-06 08:53:36 +0545

    adding the GitTutorialClass

---
+++ app/src/main/java/com/sanyasdada/catview/GitTutorialClass.java
@@ -0,0 +1,11 @@
+package com.sanyasdada.catview;
+
+public class GitTutorialClass {
+    String commit;
+    int howManyTimes;
+
+    public GitTutorialClass(String commit, int howManyTimes) {
+        this.commit = commit;
+        this.howManyTimes = howManyTimes;
+    }
+}
